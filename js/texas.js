a8ebaf839c255f685bdf875f864c5977516dcd5e
dmclough <davidclough@gmail.com>
2016-12-15 12:05:05 -0800

    starting the find card in hand, working state

--- js/texas.js
+++ js/texas.js
@@ -1,8 +1,5 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  console.log("DOM fully loaded and parsed");
-  var deckStack = createDeckStack();
-  readClickedValue();
-});
+var player1Hand = [];
+var player2Hand = [];
 
 class Card {
   constructor(value, suite) {
@@ -10,6 +7,12 @@ class Card {
     this.suite = suite;
   }
 }
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  console.log("DOM fully loaded and parsed");
+  var deckStack = createDeckStack();
+  readClickedValue();
+});
 
 function clearBoard() { //how can this be streamlined?
   while (p1hand.hasChildNodes()) {
@@ -22,30 +25,27 @@ function clearBoard() { //how can this be streamlined?
 
 //create a deck of cards
 function createDeckStack() {
+  clearBoard();
   var numberOfDecks = 1; //define the number of 52 card decks in stack.
   var deckStack = [];
-  clearBoard();
   var suitesOptions = ["heart", "spade", "club", "diamond"];
   for (var i = 0; i <= ((numberOfDecks*52) - 1); i++){
     var value = (i%13) +1;
     var suite = suitesOptions[i % 4];
     var card = new Card(value, suite);
     deckStack.push(card);
-    console.log("pushed card");
   }
 
   var m = deckStack.length, t, j;
   //Using fisher-Yates shuffle...
   while (m) {
     j = Math.floor(Math.random() * m--);
-    //ad swap it with the current element.
     t = deckStack[m];
     deckStack[m] = deckStack[j];
     deckStack[j] = t;
-    console.log("shuffled deck");
   }
-  var player1Hand = dealHand("#p1hand", deckStack);
-  var player2Hand = dealHand("#p2hand", deckStack);
+  player1Hand = dealHand("#p1hand", deckStack);
+  player2Hand = dealHand("#p2hand", deckStack);
   return deckStack;
 }
 
@@ -54,21 +54,19 @@ function dealHand(playerId,deckStack)  {
   var hand = [];
   var cardsInInitialHand = 8;
   for (var i = 0; i < cardsInInitialHand; i++) {
-    var dealt1 = deckStack.pop();
-    hand.push(dealt1);
-    $(playerId).append("<div>"+dealt1.value+"</div>");
+    var dealtCard = deckStack.pop();
+    hand.push(dealtCard);
+    $(playerId).append("<div>"+dealtCard.value+"</div>");
   }
-
-  console.log("hand", hand[1].value);
-  console.log("hand", hand);
-  console.log("new deck stack", deckStack);
-
+  console.log("hand single card", hand[1].value);
   return hand;
 }
 
 function readClickedValue() {
   $("div").click(function(e){
     var value = $(e.target).text();
+    value = parseInt(value, 10);
+
     console.log(value);
     checkClickedValue(value);
     return value; //is this needed?
@@ -76,5 +74,15 @@ function readClickedValue() {
 }
 
 function checkClickedValue(value) {
-  console.log("value2", value);
+  var card = findCardInHand(player2Hand, value);
+  console.log("clicked", value);
+  // console.log("index var", index);
+}
+
+// searches through a given hand and returns the
+// first card with the given value.
+function findCardInHand(hand, value) {
+  for (var i = 0; i < player2Hand.length; i++) {
+
+  }
 }
